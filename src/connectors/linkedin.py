d2950552923d1e845e54f0c6816531bc0f60ca69
tradingwithray777a-cna <tradingwithray777a@gmail.com>
2026-02-03 22:52:20 +0800

    Update linkedin.py

--- src/connectors/linkedin.py
+++ src/connectors/linkedin.py
@@ -1,1 +1,9 @@
+from .base import BaseConnector
+
+class LinkedinConnector(BaseConnector):
+    name = "linkedin.com/jobs/jobs-in-singapore"
+
+    def search(self, query: str, posted_within_days: int = 30):
+        # TODO: implement. Start simple with a small number of results.
+        return []
 
