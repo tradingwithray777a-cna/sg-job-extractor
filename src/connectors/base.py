c29c2505b087dff23fe83e8f61cc8e82573c086d
tradingwithray777a-cna <tradingwithray777a@gmail.com>
2026-02-03 22:40:52 +0800

    Update base.py

--- src/connectors/base.py
+++ src/connectors/base.py
@@ -1,1 +1,13 @@
+class BaseConnector:
+    name = "Base"
+
+    def search(self, query: str, posted_within_days: int = 30):
+        """
+        Return a list of dict rows matching REQUIRED_COLS (some may be Unverified).
+        Each row should contain:
+          Job title available, employer, job post url link, job post from what source,
+          date job post was posted, application closing date, key job requirement,
+          estimated salary, job full-time or part-time
+        """
+        raise NotImplementedError
 
