40db61804ceaf9ab2dc5bc48da0c636b506e1891
tradingwithray777a-cna <tradingwithray777a@gmail.com>
2026-02-03 22:45:13 +0800

    Update fastjobs.py

--- src/connectors/fastjobs.py
+++ src/connectors/fastjobs.py
@@ -1,1 +1,9 @@
+from .base import BaseConnector
+
+class FastJobsConnector(BaseConnector):
+    name = "fastjobs.sg"
+
+    def search(self, query: str, posted_within_days: int = 30):
+        # TODO: implement. Start simple with a small number of results.
+        return []
 
