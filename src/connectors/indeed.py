b97b1143a1c55ae71c89efd0ed40cd6ac38721dd
tradingwithray777a-cna <tradingwithray777a@gmail.com>
2026-02-03 22:49:43 +0800

    Update indeed.py

--- src/connectors/indeed.py
+++ src/connectors/indeed.py
@@ -1,1 +1,9 @@
+from .base import BaseConnector
+
+class IndeedConnector(BaseConnector):
+    name = "sg.indeed.com"
+
+    def search(self, query: str, posted_within_days: int = 30):
+        # TODO: implement. Start simple with a small number of results.
+        return []
 
